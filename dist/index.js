e1fbf9bf388146494fb0473e9bde8e3c221c45a3
Jerry Lee <ximpledev@gmail.com>
2018-10-23 12:06:00 +0800

    add lodash and moment for app-test to test

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,33 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 // module.exports = (name) => {
 //   return `Hi hi, ${name}!`;
 // }
+var GREET_TYPES = {
+  GREET1: 'Hello',
+  GREET2: 'Hi'
+};
 
 function greet(name) {
-  return "Hi hi, " + name + "!";
+  var key = _lodash2.default.findKey(GREET_TYPES, function (item) {
+    return item === 'Hi';
+  });
+
+  return GREET_TYPES[key] + ' ' + name + ', it\'s ' + (0, _moment2.default)().format('YYYY/MM/DD HH:mm:ss') + '!';
 }
 
 exports.default = greet;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 // module.exports = (name) => {
 //   return `Hi hi, ${name}!`;
 // }
+import _      from 'lodash';
+import moment from 'moment';
+
+const GREET_TYPES = {
+  GREET1: 'Hello',
+  GREET2: 'Hi'
+};
 
 function greet(name) {
-  return `Hi hi, ${name}!`;
+  const key = _.findKey (
+    GREET_TYPES,
+    (item) => {
+      return item==='Hi';
+    }
+  );
+
+  return `${GREET_TYPES[key]} ${name}, it's ${moment().format('YYYY/MM/DD HH:mm:ss')}!`;
 }
 
 export default greet;
